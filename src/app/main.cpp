acda8c3965e9e70e336b618d0c7846db6f308b34
FelixHommel <felixhommel04@gmail.com>
2026-06-21 03:15:03 +0200

    feat: open a window

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,4 +1,33 @@
+#include "rendering/GlfwContext.hpp"
+#include "rendering/Window.hpp"
+
+#include <exception>
+#include <memory>
+#include <print>
+#include <utility>
+
 int main()
 {
+    using namespace pen;
+
+    std::unique_ptr<GlfwContext> glfw;
+    std::unique_ptr<Window> window;
+
+    try
+    {
+        glfw = std::move(std::make_unique<GlfwContext>());
+        window = std::move(std::make_unique<Window>("ProceduralGeneration"));
+    }
+    catch(const std::exception& e)
+    {
+        std::println(stderr, "Failure while setting up GLFW:\n{}", e.what());
+        return 0;
+    }
+
+    while(!window->shouldClose())
+    {
+        GlfwContext::pollEvents();
+    }
+
     return 0;
 }
